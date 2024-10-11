02bc793990b52ce26394a218ae8d13676224c329
xawk <xawk0469@yandex.ru>
2024-10-11 16:40:48 +0300

    add unlim history remove duplicate history add history manager tests

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -13,40 +13,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class InMemoryHistoryManagerTest {
 
     private static TaskManager taskManager;
+    private static HistoryManager historyManager;
 
     @BeforeEach
     public void beforeEach() {
         taskManager = Managers.getDefault();
+        historyManager = Managers.getDefaultHistory();
     }
 
     @Test
-    public void getHistory10Tasks() {
-        for (int i = 0; i < 16; i++) {
-            taskManager.addTask(new Task(("задача " + i) , ("описание " + i)));
-        }
-
-        List<Task> tasks = taskManager.getTasks();
-        for (Task task : tasks) {
-            taskManager.getTask(task.getId());
-        }
-
-        List<Task> history = taskManager.getHistory();
-        assertEquals(10, history.size(), "Неверное количество элементов в истории ");
+    public void emptyHistory() {
+        assertEquals(0, historyManager.getHistory().size(), "История не пустая");
     }
 
     @Test
-    public void getHistory10Subtasks() {
+    public void addHistory() {
+        Task task = taskManager.addTask(new Task("Первая задача","Попить чаю", Status.NEW));
+        Task task1 = taskManager.addTask(new Task("Вторая задача","Покушать булку", Status.NEW));
         Epic epic = taskManager.addEpic(new Epic("Эпик","Большой эпик",Status.NEW));
-        for (int i = 0; i < 16; i++) {
-            taskManager.getEpicById(0).addSubtask(new Subtask(("Подзадача " + i), ("Описание " + i), epic.getId()));
-        }
+        Subtask subtask = taskManager.addSubtask(new Subtask("Под.эпик", "эпик1",epic.getId()));
+        historyManager.add(task);
+        historyManager.add(task1);
+        historyManager.add(epic);
+        historyManager.add(subtask);
+        assertEquals(4, historyManager.getHistory().size(), "История не добавлена");
+    }
 
-        List<Subtask> tasks = taskManager.getSubtasks();
-        for (Task task : tasks) {
-            taskManager.getTask(task.getId());
-        }
-
-        List<Task> history = taskManager.getHistory();
-        assertEquals(10, history.size(), "Неверное количество элементов в истории ");
+    @Test
+    public void deleteTwoHistory() {
+        Task task = taskManager.addTask(new Task("Первая задача","Попить чаю", Status.NEW));
+        Task task1 = taskManager.addTask(new Task("Вторая задача","Покушать булку", Status.NEW));
+        Epic epic = taskManager.addEpic(new Epic("Эпик","Большой эпик",Status.NEW));
+        Subtask subtask = taskManager.addSubtask(new Subtask("Под.эпик", "эпик1",epic.getId()));
+        historyManager.add(task);
+        historyManager.add(task1);
+        historyManager.add(epic);
+        historyManager.add(subtask);
+        historyManager.remove(0);
+        historyManager.remove(1);
+        assertEquals(2, historyManager.getHistory().size(), "Удалилось неверное количество историй");
+    }
+    @Test
+    public void duplicateHistory() {
+        Task task = taskManager.addTask(new Task("Первая задача","Попить чаю", Status.NEW));
+        Task task1 = taskManager.addTask(new Task("Вторая задача","Покушать булку", Status.NEW));
+        Epic epic = taskManager.addEpic(new Epic("Эпик","Большой эпик",Status.NEW));
+        Subtask subtask = taskManager.addSubtask(new Subtask("Под.эпик", "эпик1",epic.getId()));
+        historyManager.add(task);
+        historyManager.add(task1);
+        historyManager.add(epic);
+        historyManager.add(subtask);
+        historyManager.add(task);
+        historyManager.add(task1);
+        assertEquals(4, historyManager.getHistory().size(), "Имеются дубликаты в истории");
     }
 }
