689726f3a3aec337239306b03dbab890c18a471d
xawk <xawk0469@yandex.ru>
2024-10-11 16:51:42 +0300

    fix checkstyle

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         InMemoryTaskManager taskManager = new InMemoryTaskManager();
         Task task = taskManager.addTask(new Task("Первая задача","Попить чаю", Status.NEW));
         Task task1 = taskManager.addTask(new Task("Вторая задача","Покушать булку", Status.NEW));
-        System.out.println("Список всех задач"+taskManager.getAllTasks());
+        System.out.println("Список всех задач" + taskManager.getAllTasks());
 
-        System.out.println("Ищем задачу по айди первая задача = "+taskManager.getTask(0));
+        System.out.println("Ищем задачу по айди первая задача = " + taskManager.getTask(0));
         //  taskManager.removeTask(0);
         //  System.out.println("Удалили первую задачу, все задачи= " + taskManager.getAllTasks());
 
