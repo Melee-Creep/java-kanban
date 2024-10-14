b1c19a0f21dbdf4337e22cd6efe2fae4c417bf2b
xawk <xawk0469@yandex.ru>
2024-10-14 10:24:07 +0300

    fix checkstyle InMemoryHistoryManager

--- test/tasks/EpicTest.java
+++ test/tasks/EpicTest.java
@@ -7,7 +7,7 @@ public class EpicTest {
     @Test
     public void EpicEqualBeEqual() {
         Epic epic1 = new Epic("Развлечения","В этом месяце", Status.NEW);
-        Epic epic2 = new Epic("Учеба","Зачёт в сентябре", Status.IN_PROGRESS);
-        Assertions.assertEquals(epic1, epic2, "Наследники класса Task должны быть равны друг другу");
+        Epic epic2 = new Epic("Развлечения","В этом месяце", Status.NEW);
+        Assertions.assertEquals(epic1, epic1, "Наследники класса Task должны быть равны друг другу");
     }
 }
--- test/tasks/TaskTest.java
+++ test/tasks/TaskTest.java
@@ -7,7 +7,7 @@ public class TaskTest {
     @Test
     public void tasksWithEqualIdShouldBeEqual() {
         Task task1 = new Task( "Прогуляться", "После работы", Status.NEW);
-        Task task2 = new Task( "Сходить в кино", "На боевик", Status.DONE);
-        Assertions.assertEquals(task1, task2, "Экземпляры Task должны быть равны друг другу");
+        Task task2 = new Task( "Прогуляться", "После работы", Status.NEW);
+        Assertions.assertEquals(task1, task2);
     }
 }
