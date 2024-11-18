84f755297a96dc8cc8e4988d40fb71f1823528dc
xawk <xawk0469@yandex.ru>
2024-11-18 20:02:10 +0300

    fix: checkstyle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import manager.FileBackedTasksManager;
-import manager.InMemoryTaskManager;
 import tasks.Epic;
 import tasks.Status;
 import tasks.Subtask;
@@ -13,15 +12,14 @@ public class Main {
 
         File file1 = new File("File.csv");
         FileBackedTasksManager taskManager1 = new FileBackedTasksManager(file1);
-        Task newTask = taskManager1.addTask(new Task("Новая первая задача","Попить чаю", Status.NEW, 200, LocalDateTime.now()));
+        Task newTask = taskManager1.addTask(new Task("Новая первая задача","Попить чаю", Status.NEW,200, LocalDateTime.now()));
         Epic newEpic = taskManager1.addEpic(new Epic("Эпик","Большой эпик",Status.NEW));
-        Subtask newSubtask = taskManager1.addSubtask(new Subtask("Под.эпик", "эпик1", Status.DONE, 100, LocalDateTime.of(2024, 12 ,1, 12,00), newEpic.getId()));
-        Subtask newSubtask1 = taskManager1.addSubtask(new Subtask("Под.эпик2", "эпик2", Status.DONE, 10, LocalDateTime.of(2024, 12 ,1, 18,00), newEpic.getId()));
+        Subtask newSubtask = taskManager1.addSubtask(new Subtask("Под.эпик", "эпик1", Status.DONE,100, LocalDateTime.of(2024,12,1, 12,00), newEpic.getId()));
+        Subtask newSubtask1 = taskManager1.addSubtask(new Subtask("Под.эпик2","эпик2", Status.DONE,10, LocalDateTime.of(2024,12,1, 18,00), newEpic.getId()));
         FileBackedTasksManager backedTasksManager = FileBackedTasksManager.loadFromFile(file1);
         System.out.println("\n****\n backup");
         System.out.println(backedTasksManager.getAllTasks());
         System.out.println(backedTasksManager.getSubtasks());
         System.out.println(backedTasksManager.getEpics());
-
     }
 }
